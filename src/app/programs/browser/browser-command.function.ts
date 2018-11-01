57ba0a0aa57d5395501db2003fff3c562f5f3988
plunkett.bryce.m@gmail.com <plunkett.bryce.m@gmail.com>
2018-11-01 19:02:23 -0400

    fixed small typecast error that caused build error

--- src/app/programs/browser/browser-command.function.ts
+++ src/app/programs/browser/browser-command.function.ts
@@ -7,7 +7,7 @@ import { Filesystem } from '../../basic';
  * @param output - the function that can be used to output text
  * @param executeCommand  - pass a command to this function to execute it
  */
-export function browserCommand (args: Array<String>, fileSystem: Filesystem,
+export function browserCommand (args: Array<string>, fileSystem: Filesystem,
     output: Function, executeCommand: Function) {
     let fileName = args[0];
     console.log(Filesystem.getExtension(fileName));
